cba614fb6e412a1513a8d12e41facfb2d0442d32
Hunter Adelson <hunteradelson@gmail.com>
2024-02-15 22:15:01 -0500

    Setup program and starting isValidCredential function

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+//using namespace std;
+
+
+
+int main()
+{
+    
+return 0;
+}
+
+
+bool isValidCredentials() {
+
+    //Username is false or doesnt exist
+
+    //Password is false
+
+    //Password and Username Match!
+
+    return false;
+}
